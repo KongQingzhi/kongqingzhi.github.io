0519d2f8b27c2bdc8e5e8a23f4a573e2b190a2fa
孔庆志 <1428390162@qq.com>
2024-11-07 22:14:40 +0800

    chore: 集成tailwind css

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -4,4 +4,12 @@ export default {
     'stylelint-config-standard-scss',
     'stylelint-config-standard-vue',
   ],
+  rules: {
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind'],
+      },
+    ],
+  },
 };
