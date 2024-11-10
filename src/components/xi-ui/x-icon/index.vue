114829e59c707e616e9cb7dce9acface9aee8b55
孔庆志 <1428390162@qq.com>
2024-11-10 23:11:14 +0800

    feat: 新增图标组件

---
+++ src/components/xi-ui/x-icon/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg
+    class="icon"
+    aria-hidden="true"
+    :style="{ width: size, height: size, color: color }"
+  >
+    <use :xlink:href="`#icon-${name}`" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { defineOptions } from 'vue';
+defineOptions({ name: 'IconComponent' });
+defineProps({
+  color: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+  },
+});
+</script>
