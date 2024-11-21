d2b7d10f0a8b92e31642bd4422491733630e2a7d
孔庆志 <1428390162@qq.com>
2024-11-21 23:18:36 +0800

    refactor: 404背景图修改

--- src/pages/404/index.vue
+++ src/pages/404/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="relative main isolate min-h-full">
+  <main class="relative main mx-auto isolate min-h-full">
     <img
-      src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
+      src="./404.jpg"
       alt=""
-      class="absolute inset-0 -z-10 size-full object-cover object-bottom"
+      class="absolute inset-0 -z-10 size-full object-cover object-left"
     />
     <div class="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
       <p class="text-8xl font-semibold text-white">404</p>
