acaf23eb6b6f8dece552c4cfa664ea214b23c966
孔庆志 <1428390162@qq.com>
2025-04-06 21:17:18 +0800

    feat: 完善底部组件

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,6 +1,32 @@
 <template>
-  <footer class="bg-primary text-white">
-    <div class="container h-60 mx-auto p-10">111</div>
+  <footer class="bg-primary text-sm text-white">
+    <div class="container mx-auto pt-6 pb-10 px-10 space-y-4">
+      <div class="flex flex-wrap justify-between">
+        <div class="mt-4">
+          <h3 class="mb-2 text-base font-bold">联系我</h3>
+          <p>电话: 156 5375 4746</p>
+          <p>微信: 156 5375 4746</p>
+          <p>邮件: 1428390162@qq.com</p>
+        </div>
+
+        <div class="mt-4">
+          <h3 class="mb-2 text-base font-bold">我的简历</h3>
+          <a
+            href="/src/assets/index/前端开发工程师-孔庆志-15653754746.pdf"
+            target="_blank"
+            >查看我的简历</a
+          >
+        </div>
+
+        <div class="mt-4">
+          <p>未经允许,不可复制,所有解释权归创始团队</p>
+          <p class="mt-4">
+            Date:2022/07/28 address:Guizhou Normal College, 115 Wudang District,
+            Guiyang City, Guizhou Province
+          </p>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 <script setup lang="ts">
