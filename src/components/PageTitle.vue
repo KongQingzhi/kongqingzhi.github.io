20eb4465a4a8ade8adfb53f7a12ec9c6485851aa
孔庆志 <1428390162@qq.com>
2025-04-06 21:05:02 +0800

    feat: 完善 title 组件

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,5 +1,8 @@
 <template>
-  <div></div>
+  <div class="flex items-end flex-wrap py-2 my-4 border-b-2 border-primary">
+    <h3 class="text-3xl mr-8 font-bold text-center text-block">{{ title }}</h3>
+    <p class="text-center text-gray-400">{{ aside }}</p>
+  </div>
 </template>
 
 <script setup lang="ts">
