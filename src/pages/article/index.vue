719a69745f815dc5a91838479e937aaacf7e44da
孔庆志 <1428390162@qq.com>
2025-04-07 21:45:28 +0800

    feat: 完善文章页面

--- src/pages/article/index.vue
+++ src/pages/article/index.vue
@@ -1,7 +1,49 @@
 <template>
-  <div></div>
+  <section>
+    <PageTitle title="博客文章" aside="积累错误，总结经验。" />
+    <div class="space-y-4 mb-10">
+      <ArticleTItle
+        v-for="item in articleList"
+        :key="item.title"
+        :article="item"
+      />
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
+import PageTitle from '@/components/PageTitle.vue';
+import ArticleTItle from '@/components/ArticleTItle.vue';
 defineOptions({ name: 'ArticlePage' });
+
+const articleList = [
+  {
+    path: '',
+    title: '前端工程化',
+  },
+  {
+    path: '',
+    title: 'CSS 三栏布局完全指南',
+  },
+  {
+    path: '',
+    title: 'VitePress 中集成 Tailwind CSS',
+  },
+  {
+    path: '',
+    title: '如何在 vue 项目中使用阿里巴巴图标库并封装成组件',
+  },
+  {
+    path: '',
+    title: '解决 eslint 无法识别全局类型的问题',
+  },
+  {
+    path: '',
+    title: '微信小程序订阅消息',
+  },
+  {
+    path: '',
+    title: 'HTML 基础',
+  },
+];
 </script>
