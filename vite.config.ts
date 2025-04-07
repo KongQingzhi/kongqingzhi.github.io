502480f611b41eb0c3c88ae680c289d419452a44
孔庆志 <1428390162@qq.com>
2025-04-07 22:23:01 +0800

    fix: 修复页面刷新 404 的问题

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/dist/',
   plugins: [vue()],
   resolve: {
     alias: {
